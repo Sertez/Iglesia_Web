20ce93e238c10005382a025c36c9786e6dd34c8f
Santiago26009 <39531781+Santiago26009@users.noreply.github.com>
2018-05-23 16:10:47 -0500

    Add files via upload

---
+++ TOV/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+app.set("view engine","jade");
+
+app.use(express.static("public"));
+
+app.get("/",function(req, res){
+	res.render("index")
+});
+
+
+app.listen(8080);
