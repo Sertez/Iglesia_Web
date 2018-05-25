cc992da569fd2c45ae356b45c241fef34f88134b
SertesX <danieldoav@gmail.com>
2018-05-24 22:02:54 -0500

    Empezado
    Pues, las imagenes estan añadidas en las carpeta assets/imgs. nuevos css, pagina principal. los links no sirven para ir a otras paginas no sirven y falta aclarar bien el diseño de la interfaz

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 
 var app = express();
+
 app.set("view engine","jade");
+
+app.use(express.static("assets"));
 
 app.get("/",function(solicitud,respuesta){
 	respuesta.render("index");
