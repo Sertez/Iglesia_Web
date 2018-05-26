d784cf8a8eded7f56232add27c3124d7499ca335
SertesX <danieldoav@gmail.com>
2018-05-26 11:14:33 -0500

    Links
    Ya sirven los links, se cambio la fuente, los colores y se añadio un calendario en el que hay algunos errores

--- app.js
+++ app.js
@@ -10,4 +10,21 @@ app.get("/",function(solicitud,respuesta){
 	respuesta.render("index");
 });
 
+app.get("/sesiones",function(solicitud,respuesta){
+	respuesta.render("sesiones");
+});
+
+app.get("/quienes-somos",function(solicitud,respuesta){
+	respuesta.render("quienes-somos");
+});
+
+app.get("/que-hacemos",function(solicitud,respuesta){
+	respuesta.render("que-hacemos");
+});
+
+app.get("/nuestro-fundador",function(solicitud,respuesta){
+	respuesta.render("nuestro-fundador");
+});
+
+
 app.listen(8080);
